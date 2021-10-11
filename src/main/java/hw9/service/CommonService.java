4a7b99dcc33d17b05627f61ede95045b4e89c8ac
Aleksandr Iablonskikh <Aleksandr_Iablonskikh@epam.com>
2021-10-11 13:59:41 +0300

    no comments

--- src/main/java/hw9/service/CommonService.java
+++ src/main/java/hw9/service/CommonService.java
@@ -31,17 +31,6 @@ public class CommonService extends URI {
             .build();
     }
 
-//    public CommonService() {
-//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-//        REQUEST_SPECIFICATION = new RequestSpecBuilder()
-//            .setBaseUri(URI)
-//            .setContentType(ContentType.JSON)
-//            .addQueryParam("key", PropertiesReader.getProperty("apiKey"))
-//            .addQueryParam("token", PropertiesReader.getProperty("apiToken"))
-//            .addFilter(new ResponseLoggingFilter())
-//            .build();
-//    }
-
     public Response getNoParams(Method method, String path) {
 
         return given()
@@ -53,23 +42,6 @@ public class CommonService extends URI {
             .extract()
             .response();
     }
-
-//    public Response getNoParams(String uri) {
-//        Response response = given(REQUEST_SPECIFICATION).get(uri);
-//        response.then()
-//                .statusCode(Matchers.lessThan(300))
-//                .statusCode(Matchers.greaterThanOrEqualTo(200));
-//        return response;
-//    }
-//
-//    public Response getWithParams(String uri, Map<String, Object> params) {
-//        RequestSpecification specification = given(REQUEST_SPECIFICATION);
-//
-//        for (Map.Entry<String, Object> param : params.entrySet())
-//            specification.param(param.getKey(), param.getValue());
-//
-//        return specification.get(uri);
-//    }
 
     public Response getWithParams(Method method, String path, Map<String, String> parameters) {
 
